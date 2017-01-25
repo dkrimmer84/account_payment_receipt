2e4397be32934ec853dce85d33576e1051c5563f
maye <mrel_92@hotmail.com>
2017-01-25 10:58:02 -0500

    Comprobante de ingreso y egreso

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Probuse Consulting Service Pvt Ltd. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Inherit Account Print pyment',
+    'version' : '1.0',
+    'author' : '',
+    'license': '',
+    'category' : 'Accounting & Finance',
+    'summary': 'Inherit - Account Pyments - Odoo 9.0',
+    'description' : """
+
+    """,
+    'website': '',
+    'depends' : ['account'],
+    'data': ['report/payment_report.xml'],
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

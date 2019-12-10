18b9bb85f46701edd09f55258614eba571814217
Jhony Marcatoma <jjmarcatoma@gmail.com>
2019-12-10 01:10:49 -0500

    [FIX]. Corrige reporte de comprobante de pago

--- __manifest__.py
+++ __manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Probuse Consulting Service Pvt Ltd. See LICENSE file for full copyright and licensing details.
 {
     'name' : 'Inherit Account Print pyment',
@@ -6,7 +5,7 @@
     'author' : '',
     'license': '',
     'category' : 'Accounting & Finance',
-    'summary': 'Inherit - Account Pyments - Odoo 9.0',
+    'summary': 'Inherit - Account Pyments - Odoo 12.0',
     'description' : """
 
     """,
@@ -15,5 +14,3 @@
     'data': ['report/payment_report.xml'],
     'installable': True,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
